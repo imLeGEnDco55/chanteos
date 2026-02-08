4a41c2ea82becc5fe6208bf3038fcf38a4b5d236
imLeGEnDco55 <imlegendco@gmail.com>
2026-02-08 00:19:03 -0600

    feat: migrate to gemini and setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: "/chanteos/",
   plugins: [
     react(),
     mode === "development" && componentTagger(),
